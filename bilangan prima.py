86a64ef58deac795261b2a06ce3f25eb088a9bdf
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2018-08-28 09:17:21 +0700

    Create bilangan prima.py

---
+++ bilangan prima.py
@@ -0,0 +1,11 @@
+# tentukan batas bilangan prima yang akan dicari
+Angka = int(input('Jumlah bilangan : '))
+
+#perulangan untuk mengecek bilangan prima
+for num in range(2,Angka):
+    prima = True
+    for i in range(2,num):
+        if (num%i==0):
+            prima = False
+    if prima:
+       print (num)
