051f0842b3a04f474886127dc3f60b601642539c
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2018-08-28 11:15:36 +0700

    Create bilangan selanjutnya *8.py

---
+++ bilangan selanjutnya *8.py
@@ -0,0 +1,11 @@
+angka = int(input('Jumlah bilangan : '))
+a = int(input("angka awal : "))
+x = 0;
+awal = a;
+while (x < angka):
+	keN = 8 * awal;
+	nextnum = awal;
+	awal = keN;
+	print(nextnum)
+	x += 1;
+	pass
