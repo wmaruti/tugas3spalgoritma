f07e7c6a6158c3283b2b45996ef5f0982d1e17a0
Wahyu Maruti Adjie <32800189+wmaruti@users.noreply.github.com>
2018-08-28 10:25:01 +0700

    Update bilangan prima.py

--- bilangan prima.py
+++ bilangan prima.py
@@ -2,7 +2,7 @@
 Angka = int(input('Jumlah bilangan : '))
 
 #perulangan untuk mengecek bilangan prima
-for num in range(2,Angka):
+for num in range(2,42):
     prima = True
     for i in range(2,num):
         if (num%i==0):
